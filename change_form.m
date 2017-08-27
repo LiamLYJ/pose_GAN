4c40370492201f6197ccef0ceaa5fb9aebb3e191
LiamLYJ <liuyongjie9313@yahoo.co.jp>
2017-08-27 16:23:49 +0900

    update with 256 resize

--- change_form.m
+++ change_form.m
@@ -52,66 +52,66 @@
 % lsp_gt(:,:,:) = data(:,:,1:1000);
 % save('lsp_test_1000.mat','lsp_gt');
 
-% tmp = load('/home/hpc/ssd/lyj/liu_data/action_training_nobg.mat');
-% data = tmp.posAction;
-% dataset = [];
-% dataset.image = [];
-% dataset.size = [];
-% dataset.joints = [];
-% count = 0;
-% dict = [13,12,8,7,6,2,1,0,9,10,11,3,4,5];
-% file_prefix = '/home/hpc/ssd/lyj/liu_data/';
-% 
-% for num_list = 1:length(data)
-%     for num_file = 1:length(data{1,num_list})
-%         count = count + 1;
-%         dataset(count).image = [file_prefix,data{1,num_list}(num_file).fileName];
-%         
-% %         dataset(count).size = [3,data{1,num_list}(num_file).height,data{1,num_list}(num_file).width];
-%         h = data{1,num_list}(num_file).height; 
-%         w = data{1,num_list}(num_file).width; 
-%         x_scale = 256 / w; 
-%         y_scale = 256 / h;
-%         dataset(count).size = [3,256,256];
-%         
-%         cur_point = data{1,num_list}(num_file).point;
-%         tmp = zeros(14,3);
-%         for num_joint = 1:14
-% %             tmp(num_joint,:) = [dict(num_joint),cur_point(num_joint,1),cur_point(num_joint,2)];
-%             tmp(num_joint,:) = [dict(num_joint),cur_point(num_joint,1) * x_scale,cur_point(num_joint,2)*y_scale];
-%         end
-%         dataset(count).joints{1} = tmp;
-%     end
-% end
-% save('cus_train.mat','dataset')
-
-
-tmp = load('/home/hpc/ssd/lyj/liu_data/action_testing_nobg.mat');
-dict = [14,13,9,8,7,3,2,1,10,11,12,4,5,6];
+tmp = load('/home/hpc/ssd/lyj/liu_data/action_training_nobg.mat');
 data = tmp.posAction;
+dataset = [];
+dataset.image = [];
+dataset.size = [];
+dataset.joints = [];
 count = 0;
+dict = [13,12,8,7,6,2,1,0,9,10,11,3,4,5];
+file_prefix = '/home/hpc/ssd/lyj/liu_data/';
+
 for num_list = 1:length(data)
     for num_file = 1:length(data{1,num_list})
-        cur_point = data{1,num_list}(num_file).point;
-        tmp = zeros(14,2);
+        count = count + 1;
+        dataset(count).image = [file_prefix,data{1,num_list}(num_file).fileName];
         
+%         dataset(count).size = [3,data{1,num_list}(num_file).height,data{1,num_list}(num_file).width];
         h = data{1,num_list}(num_file).height; 
         w = data{1,num_list}(num_file).width; 
         x_scale = 256 / w; 
         y_scale = 256 / h;
-
+        dataset(count).size = [3,256,256];
+        
+        cur_point = data{1,num_list}(num_file).point;
+        tmp = zeros(14,3);
         for num_joint = 1:14
-%             tmp(dict(num_joint),1:2) = cur_point(num_joint,1:2);
-            tmp(dict(num_joint),1) = cur_point(num_joint,1) * x_scale;
-            tmp(dict(num_joint),2) = cur_point(num_joint,2) * y_scale;
+%             tmp(num_joint,:) = [dict(num_joint),cur_point(num_joint,1),cur_point(num_joint,2)];
+            tmp(num_joint,:) = [dict(num_joint),cur_point(num_joint,1) * x_scale,cur_point(num_joint,2)*y_scale];
         end
-        if count == 0
-            gt = tmp;
-        else
-            gt = cat(3,gt,tmp);
-        end
-        count = count + 1;
+        dataset(count).joints{1} = tmp;
     end
 end
-% save('test_cus_original.mat','gt');
-save('test_cus_256.mat','gt');
+save('cus_train.mat','dataset')
+
+
+% tmp = load('/home/hpc/ssd/lyj/liu_data/action_testing_nobg.mat');
+% dict = [14,13,9,8,7,3,2,1,10,11,12,4,5,6];
+% data = tmp.posAction;
+% count = 0;
+% for num_list = 1:length(data)
+%     for num_file = 1:length(data{1,num_list})
+%         cur_point = data{1,num_list}(num_file).point;
+%         tmp = zeros(14,2);
+%         
+%         h = data{1,num_list}(num_file).height; 
+%         w = data{1,num_list}(num_file).width; 
+%         x_scale = 256 / w; 
+%         y_scale = 256 / h;
+% 
+%         for num_joint = 1:14
+% %             tmp(dict(num_joint),1:2) = cur_point(num_joint,1:2);
+%             tmp(dict(num_joint),1) = cur_point(num_joint,1) * x_scale;
+%             tmp(dict(num_joint),2) = cur_point(num_joint,2) * y_scale;
+%         end
+%         if count == 0
+%             gt = tmp;
+%         else
+%             gt = cat(3,gt,tmp);
+%         end
+%         count = count + 1;
+%     end
+% end
+% % save('test_cus_original.mat','gt');
+% save('test_cus_256.mat','gt');
